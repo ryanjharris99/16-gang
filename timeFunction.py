a9ab98a014d18a507bc2ae87a6dd98ba85992fff
Gabe Whale <gabewhale@gmail.com>
2017-10-17 14:32:11 +0100

    Returns Time Since Event

---
+++ timeFunction.py
@@ -0,0 +1,19 @@
+#This is the function which returns a time and can be used to 
+import time 
+
+
+
+def getCurrentTime ():
+	"""This function returns the time as a Float.
+	It will be useful for time critical incidents.
+	For example time in the xRay machine.
+	Or time whilst fighting the zombies."""
+	return time.time()
+
+
+initTime = getCurrentTime() #this would be then time you enter x-ray room
+
+while True:
+	currentTime = getCurrentTime()         #updates the current time
+	timeSinceInit = currentTime - initTime #this calcualtes the time since initial event
+	print(timeSinceInit)				   #prints the time
