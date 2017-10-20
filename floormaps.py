4624abb43b30f7da78692c5672c490433c05c097
AkanshaGarg198 <32823996+AkanshaGarg198@users.noreply.github.com>
2017-10-20 17:57:46 +0100

    floormaps

---
+++ floormaps.py
@@ -0,0 +1,98 @@
+room_OT={
+	"name": "Operating Theatre",
+
+	"description": 
+	 """You go into the Operating Theartre in front of you is a half operated on body, chest wide open with a scalpel inserted between the fourth and fifth rib.  
+	 There are dozens of surgical drape sheets scattered around the room as if a savage animal had been set aloose... 
+	 You walk up to the body and are fixated upon the fresh bullet hole between the victims eyes. 
+	 There are also unused bullets by the victims head.
+	 You look about more and there are plenty of surgial cuboards for you to look through.
+	 """,
+
+	 "exits": {}
+
+}
+
+room_Waiting={
+	"name": "Waiting Room",
+
+	"description":
+	"""You walk in and there is an eerie silence in the derelict room, nothing to search through only rows of empty seats..."""
+
+	"exits": {}
+	
+}
+
+room_Xray={
+	"name": "Xray Room"
+
+	"description":
+	"""Immediately after entering the room you are greeted by the humming of grand x-ray machines and a dingey ultra green light...
+	The machines are battered and on however there is no obvious way of turning it off especially as the contorl panal is damaged.
+	With each step around the room the faint sound of crunching paper arises above the humming of machines. 
+	You then realise there are research notes all over the floor.
+	Could there be any useful information amongst the scattered notes?
+
+	  """
+
+	"exits": {}
+}
+
+room_reception={
+	"name": "Reception"
+
+	"description":
+	"""    """
+
+	"exits": {}
+
+}
+
+room_attic={
+	"name": "Attic"
+
+	"description": 
+	"""    """
+
+	"exits": {}
+}
+
+room_morgue={
+	"name": "Morgue"
+
+	"description":
+	""" """
+
+	"exits": {}
+}
+
+room_ChildrenWard={
+	"name": "The Children's Ward"
+
+	"description":
+	""" """
+
+	"exits": {}
+
+}
+
+room_PatientRoom={
+	"name": "The Patient's Room"
+
+	"description":
+	""" """
+
+	"exits": {}
+}
+
+rooms ={
+"OT": room_OT,
+"Waiting_Room": room_Waiting,
+"Xray": room_Xray,
+"Reception": room_reception,
+"Attic": room_attic,
+"Morgue": room_morgue,
+"Children_Ward": room_ChildrenWard
+"Patient_Room": room_PatientRoom
+	
+}
