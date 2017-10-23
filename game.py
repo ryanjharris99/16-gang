3943c3052106d4a69c512812327c0e4c5f6ae786
Ryan Harris <ryanharris6321@gmail.com>
2017-10-23 14:54:33 +0100

    Not sure what I did

--- game.py
+++ game.py
@@ -328,7 +328,7 @@ def main():
 
 
 
-# Are we being run as a script? If so, run main().
+# Are we being run as a s`ipt? If so, run main().
 # '__main__' is the name of the scope in which top-level code executes.
 # See https://docs.python.org/3.4/library/__main__.html for explanation
 if __name__ == "__main__":
