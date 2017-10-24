c56b1ee1a9eeba6e0f2c8e603c59b13d42aca480
AkanshaGarg198 <32823996+AkanshaGarg198@users.noreply.github.com>
2017-10-24 12:49:10 +0100

    prints messages every few seconds

--- Game/timeFunction.py
+++ Game/timeFunction.py
@@ -1,6 +1,38 @@
-#This is the function which returns a time and can be used to 
+#used this website https://automatetheboringstuff.com/chapter15/
 import time 
+import random
 
+
+def main ():
+	initial = getCurrentTime ()
+
+	difficult = 1
+	"""
+	if (difficulty == "easy"):
+		difficult = 1
+	elif (difficulty == "medium"):
+		difficult == 1.5
+
+	elif (difficulty == "hard"):
+		difficult == 2
+
+	"""
+
+	for i in range (4):
+		printMessage()
+		time.sleep(15*difficult)
+
+	
+	print("Oh no! you stayed too long inside the room, you couldnt survive...")	
+	
+
+	
+
+
+def printMessage ():
+#https://stackoverflow.com/questions/306400/how-to-randomly-select-an-item-from-a-list
+	Messages = ["You're starting to feel drowsy...", "You're Hands are starting to sweat...","something doesn't feel right...","You can start to feel weak..."]
+	print(random.choice(Messages))
 
 
 def getCurrentTime ():
@@ -10,5 +42,13 @@ def getCurrentTime ():
 	Or time whilst fighting the zombies."""
 	return time.time()
 
-def timeSince(initial, current):
+def timeSince(initial):
+	current = getCurrentTime()
+
+	print (current)
+
+	print (initial)
+
 	return current - initial
+
+main ()
