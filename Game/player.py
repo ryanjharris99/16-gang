ef8e7dfc70a83f063f62efca424d80d6cfe95821
Ryan Harris <ryanharris6321@gmail.com>
2017-10-24 16:07:41 +0100

    Started at patients room

--- Game/player.py
+++ Game/player.py
@@ -1,9 +1,9 @@
 from items import *
 from map import rooms
 
-inventory = [item_patients_gown]
+inventory = [item_patients_gown, item_parachute]
 energy = 10 #The energy level of the player
 moved = True #Did the player move room last turn
 
 # Start game at the reception
-current_room = rooms["Attic"]
+current_room = rooms["Patients_Room"]
