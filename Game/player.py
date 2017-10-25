f91aad46bbec6a334a68c0ddcc4009cd8891f9e3
Gabe Whale <gabewhale@gmail.com>
2017-10-25 15:16:29 +0100

    Difficulty now effects infection chance.

--- Game/player.py
+++ Game/player.py
@@ -6,6 +6,6 @@ inventory = [item_patients_gown, item_parachute]
 energy = 10 #The energy level of the player
 moved = True #Did the player move room last turn
 player_hp = 50 
-difficulty = ""
+difficulty = "easy"
 # Start game at the reception
 current_room = rooms["Patient_Room"]
