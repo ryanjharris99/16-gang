fa9dbfc806082be34bf15db63899e0ce5fdda7ad
Gabe Whale <gabewhale@gmail.com>
2017-10-25 17:51:02 +0100

    Endings finished now...

--- Game/endings.py
+++ Game/endings.py
@@ -3,6 +3,10 @@ from gameparser import *
 import player
 global inventory
 global hp
+import time
+
+
+
 
 def checkEndings(current_room, command):
 	if current_room["name"] == "Attic" and command[0] == "jump":
@@ -25,26 +29,39 @@ def died():
 	type_print("You have died!")
 
 def receptionEnding():
-	type_print("""You leave the reception and you’re greeted by the blinding light of day, a light you thought you’d never see. 
-		You tall fences all surrounding the hospital. You then spot police lights flashing behind those fences,
-		 you stagger towards the lights exhausted from the happenings within the derelict hospital. 
-You are greeted at the fence by police officers clothed in massive yellow suits with the writing “BIOHAZARD” all over them.
- One of the men asked whilst looking astonished: “Where did you come from?”
-  You are grabbed forcefully by the men and a sample of your blood is taken. 
-  Several seconds pass with the man gazing at the device he’s using to examine your blood. 
- The machine beeps and…
-""")
+	texty = ["You leave the reception and you are greeted by the blinding light of day.",
+	 "A light you thought you'd never see.",
+		"You see tall fences all surrounding the hospital.",
+		"You then spot police lights flashing behind those fences,",
+		"you stagger towards the lights exhausted from the happenings within the derelict hospital.",
+		"You are greeted at the fence by police officers clothed in massive yellow suits with the writing 'BIOHAZARD' all over them.",
+ 		"One of the men asked whilst looking astonished: 'Where did you come from?'",
+  		"You are grabbed forcefully by the men and a sample of your blood is taken.",
+  		"Several seconds pass with the man gazing at the device he's using to examine your blood.",
+ 		"The machine beeps and..."]
+	for i in range (0, len(texty)):
+		type_print(texty[i])
+		time.sleep(1)
+
+
+	
 
 	if player.infected == True:
-		type_print(""" The officer’s face drops and he goes for his gun. """)
+		type_print("""The officer's face drops and he goes for his gun. """)
 		choice = input("What do you do? (Run/Attack/Scream)")
-		type_print("Whilst attempting to " + choice + """way the officer unsheathes his weapon and places it between your eyes before uttering the words:
-		 “I’m so sorry.” 
-		 BANG. 
-		 You came so close but so far.
-		 You Die.
 
+		type_print("You try to " + choice + " but the officer was to quick and says:")
 
+		texty = ["I'm so sorry.", 
+		"BANG. ",
+		"You came so close but so far.",
+		"You Die."]
+		for i in range (0, len(texty)):
+			type_print(texty[i])
+			time.sleep(1)
+			print()
+
+		print("""
   ________                                                 ._.
  /  _____/_____    _____   ____     _______  __ ___________| |
 /   \  ___\__  \  /     \_/ __ \   /  _ \  \/ // __ \_  __ \ |
@@ -54,8 +71,8 @@ You are greeted at the fence by police officers clothed in massive yellow suits 
 """) 
 
 	else:
-		type_print("The officer smiles and says “He’s clean. Patch him up and get him some water, he’s been through a lot…”")
-		print(""" 
+		type_print("The officer smiles and says 'He's clean. Patch him up and get him some water, he's been through a lot...'")
+		type_print(""" 
 _____.___.               __      __.__         ._.
 \__  |   | ____  __ __  /  \    /  \__| ____   | |
  /   |   |/  _ \|  |  \ \   \/\/   /  |/    \  | |
@@ -63,5 +80,4 @@ _____.___.               __      __.__         ._.
  / ______|\____/|____/    \__/\  / |__|___|  /  __
  \/                            \/          \/   \/
 """)
-
 
