1dfe60032ea09b3cac7eab1eb7c3eb9a57ffba4b
Gabe Whale <gabewhale@gmail.com>
2017-10-25 15:02:23 +0100

    Chance of infection when zombie strikes.

--- Game/player.py
+++ Game/player.py
@@ -1,6 +1,7 @@
 from items import *
 from map import rooms
 
+infected = False 
 inventory = [item_patients_gown, item_parachute]
 energy = 10 #The energy level of the player
 moved = True #Did the player move room last turn
